81f530822f6d589d314baf03cb77cd1deec93a23
stens0n <astenson010@gmail.com>
2023-02-05 06:50:42 -0500

    first

---
+++ ifElse.js
@@ -0,0 +1,12 @@
+//Q: Q: What is the syntax of an if/else statement in JavaScript?
+
+if (condition) {
+    // code to be executed if the condition is true
+} else {
+    // code to be executed if condition is false
+}
+
+// The "condition" is an expression that is evaluted to either "true" 
+// or "false". The code inside the curly braces "{}" following the "if" 
+// keyword will be executed if the "condition" is true, and the code inside 
+// the curly braces following the "else" keyword will be executed if the condition is false.
